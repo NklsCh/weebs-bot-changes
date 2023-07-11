029b84a5a10ff4561d6ff7a5c4bc247f6aac6bb0
weebwashere <128880884+weebwashere@users.noreply.github.com>
2023-07-11 09:43:26 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const { Client } = require("guilded.js");
+const { Collection } = require("discord.js");
+const { token, prefix, color, ownerId, mongoURI } = require("./settings.json");
+const functions = require("./handlers/functions");
+const client = new Client({token : token});
+
+const express = require('express');
+const app = express();
+const port = 5000;
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+  });
+  
+client.commands = new Collection();
+client.aliases = new Collection();
+client.settings = { prefix, color, ownerId };
+client.functions = functions;
+
+for(let handler of  ["command", "event"]) require(`./handlers/${handler}`)(client);
+
+client.login();
+
+app.listen(port, () => {
+  console.log(`purp is listening to port : ${port}`)
+  
+})
