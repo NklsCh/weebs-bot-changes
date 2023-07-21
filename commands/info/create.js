3e0725d15fc12e8538b40816b88de4b7f5cbc50d
weebwashere <128880884+weebwashere@users.noreply.github.com>
2023-07-21 20:36:32 +0000

    stuff ag

--- commands/info/create.js
+++ commands/info/create.js
@@ -28,61 +28,62 @@ module.exports = {
     try {
       const userId = message.author.id;
       const time = args[0];
-      const reminder = args.slice(1).join(" ") || "Reminder";
+      const reminderText = args.slice(1).join(" ");
+      const reminder = reminderText ? `${reminderText}` : "";
       const option = args.find((arg) => arg === "private" || arg === "silent");
-
+  
       if (!time) {
-        return message.reply("please use the format \`.create [time] [silent | private]\` when creating reminders. Thank you.");
+        return message.reply({content: "please use the format \`.create [time] [silent | private]\` when creating reminders. Thank you.", isSilent: true})
       }
-
+  
       const timeMs = ms(time);
       if (!timeMs) {
-        return message.reply("please use the format \`.create [time] [silent | private]\` when creating reminders. Thank you.");
+        return message.reply({content: "please use the format \`.create [time] [silent | private]\` when creating reminders. Thank you.", isSilent: true})
       }
-
+  
       if (timeMs < 60000 || timeMs > 2592000000) {
-        return message.reply("please set a reminder time between 1 minute and 30 days.");
+        return message.reply({content: "please set a reminder time between 1 minute and 30 days.", isSilent: true})
       }
-
+  
       const cooldown = cooldowns.get(userId);
       if (cooldown && (Date.now() - cooldown) < 60000) {
         const remainingTime = ms(60000 - (Date.now() - cooldown), { long: true });
-        return message.reply(`please wait ${remainingTime} before setting another reminder.`);
+        return message.reply({content: `please wait ${remainingTime} before setting another reminder.`, isSilent: true})
       }
-
+  
       const reminderObj = {
         _id: uuidv4().replace(/-/g, "").substr(0, 12),
         userId,
         reminder,
         time: new Date(Date.now() + timeMs),
       };
-
+  
       const result = await db.collection(MONGODB_COLLECTION_NAME).insertOne(reminderObj);
       const reminderId = reminderObj._id;
-
+  
       const embed = new Embed()
         .setColor("#F5C400")
         .setTitle("Done!")
-        .setDescription(`Your reminder has now been made. I will remind you in \`${time}\``.)
+        .setDescription(`Your reminder has now been made. I will remind you in the following: \`\`\`${time}\`\`\` with the following message: \`\`\`${reminder}\`\`\``)
         .setFooter(`Reminder ID: ${reminderId}`);
-
+  
       message.reply({ embeds: [embed], isSilent: true })
         .then((msg) => {
           msg.addReaction("90002171");
         });
-
+  
       cooldowns.set(userId, Date.now());
-
+  
       setTimeout(async () => {
         const exists = await db.collection(MONGODB_COLLECTION_NAME).findOne({ _id: reminderId });
         if (exists) {
           db.collection(MONGODB_COLLECTION_NAME).deleteOne({ _id: reminderId });
-
+  
           const embed = new Embed()
             .setColor("#F5C400")
             .setTitle("Time's Up!")
-            .setDescription(`hey there! (<@${userId}>) your reminder is up! you have now been reminded.`)
-
+            .setDescription(`<@${userId}> your time is up! You have now been reminded with the following message: \`\`\`${reminder}\`\`\` wanna make another one? do \`.create [time] [message] [private or silent]\``)
+  
           if (option === "private") {
             message.reply({ embeds: [embed], isPrivate: true })
               .then((msg) => {
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const client = new Client({token : token});
 
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = 3000;
 
 // Serve static files from the "public" directory
 app.use(express.static('public'));
